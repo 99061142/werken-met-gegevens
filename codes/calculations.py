45b52616a82c3bee78867ec0bf24c4b282eb8976
99061142 <99061142@mydavinci.nl>
2021-09-24 14:19:00 +0200

    Code + screenshots of code

---
+++ codes/calculations.py
@@ -0,0 +1,24 @@
+#Lunch
+crossaints = 17 * .39
+bread = 2 * 2.78
+discount = 3 * .5
+
+price = crossaints + bread
+lunchTotal = price - discount
+
+print(lunchTotal)
+# /Lunch
+
+# Arcade
+persons = 3
+ticketTotal = persons * 7.45 
+
+gameseat_time = 45;
+
+gameseat_price = (gameseat_time / 5) * .37
+gameseatTotal = gameseat_price * persons
+
+arcadeTotal = gameseatTotal + ticketTotal
+
+print(arcadeTotal)
+# /Arcade
